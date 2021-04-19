375bb5b98a6d848cc65377a83995c0487f101535
elifozerr <elifozerr1@outlook.com>
2021-04-19 13:48:13 +0300

    destroy_mem

---
+++ destory_memory.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <math.h>
+#include <semaphore.h>
+#include "MemTree.c"
+#include "sbmem.c"
+
+int main(){
+
+
+    sbmem_remove();
+}
