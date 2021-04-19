bdecc893a6991eaf4bf63c971899131e6f450bbd
ardaakcabuyuk <ardaakcabuyuk@gmail.com>
2021-04-19 03:04:36 -0700

    debuggable app

--- app.c
+++ app.c
@@ -8,19 +8,26 @@ int main(){
 
     int i, ret;
     char *p;
+    printf("Before sbmem_open\n");
     ret = sbmem_open();
+    printf("After sbmem_open\n");
     if (ret == -1)
         exit (1);
 
+    printf("Before sbmem_alloc\n");
     p = sbmem_alloc (ASIZE);
+    printf("After sbmem_alloc\n");
     // allocate space to forcharacters
     for (i = 0; i < ASIZE; ++i)
       p[i] = 'a';
 
     // init all chars to ‘a’
 
+    printf("Before sbmem_alloc\n");
     sbmem_free (p);
+    printf("After sbmem_free\n");
     sbmem_close();
+    printf("After sbmem_close\n");
     return (0);
 
 }
